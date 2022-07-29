0ec41710cb3fe53eaa7b617e003e6edc661f3b6d
wiktfic226 <wiktoriafica00@gmail.com>
2022-07-29 14:11:37 +0200

    feat: add dlq consumer and comments

--- src/main/java/pl/fis/rabbitmq/config/GlobalConsumerConfig.java
+++ src/main/java/pl/fis/rabbitmq/config/GlobalConsumerConfig.java
@@ -1,6 +1,7 @@
 package pl.fis.rabbitmq.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,27 +13,36 @@ import java.util.function.Consumer;
 public class GlobalConsumerConfig {
     @Bean
     public Consumer<String> createdConsumer(Tracer tracer) {
-        return (msg) -> log.info("<TRACE:{}> CREATED - Received message: {}", tracer.currentSpan().context().traceId(), msg);
+        return (msg) -> log.info("CREATED - Received message: {}, <TRACE:{}> ", msg, tracer.currentSpan().context().traceId());
     }
 
     @Bean
     public Consumer<String> updatedConsumer(Tracer tracer) {
-        return (msg) -> log.info("<TRACE:{}> UPDATED - Received message: {}", tracer.currentSpan().context().traceId(), msg);
+        return (msg) -> log.info("UPDATED - Received message: {}, <TRACE:{}> ", msg, tracer.currentSpan().context().traceId());
     }
 
     @Bean
     public Consumer<String> deletedConsumer(Tracer tracer) {
-        return (msg) -> log.info("<TRACE:{}> DELETED - Received message: {}", tracer.currentSpan().context().traceId(), msg);
+        return (msg) -> log.info("DELETED - Received message: {}, <TRACE:{}> ", msg, tracer.currentSpan().context().traceId());
     }
 
     @Bean
     public Consumer<String> emailConsumer(Tracer tracer) {
-        return (msg) -> log.info("<TRACE:{}> EMAIL - Received message: {}", tracer.currentSpan().context().traceId(), msg);
+        return (msg) -> log.info("EMAIL - Received message: {}, <TRACE:{}> ", msg, tracer.currentSpan().context().traceId());
     }
 
     @Bean
     public Consumer<String> modelConsumer(Tracer tracer) {
-        return (msg) ->
-                log.info("<TRACE:{}> Received message: {}", tracer.currentSpan().context().traceId(), msg);
+        return (msg) -> log.info("Received message: {}, <TRACE:{}> ", msg, tracer.currentSpan().context().traceId());
+       // Uncomment to force message to fail and sent it to DLQ
+//        {
+//            throw new AmqpRejectAndDontRequeueException("failed");
+//        };
     }
+
+    @Bean
+    public Consumer<String> dlqConsumer(Tracer tracer) {
+        return (msg) -> log.info("DLQ - Received message from DLQ: {}", msg);
+    }
+
 }
